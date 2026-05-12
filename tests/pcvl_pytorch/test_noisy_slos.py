7935e0f2fb91b6a205bb8a2dce008c05e666c1d0
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2026-05-12 16:02:36 -0400

    Missing the merging of the previous task

--- tests/pcvl_pytorch/test_noisy_slos.py
+++ tests/pcvl_pytorch/test_noisy_slos.py
@@ -3,14 +3,8 @@ import torch
 from perceval.utils import BasicState, StateVector
 
 from merlin import ComputationSpace
-from merlin.pcvl_pytorch.noisy_slos import _InputStateNoisySLOSComputeGraph
-
-
-def test_noisy_slos():
-    a = _InputStateNoisySLOSComputeGraph(
-        [2, 0, 1, 3, 1],
-        indistinguishability=0.2,
-        computation_space=ComputationSpace.UNBUNCHED,
-    )
-    print(a.computation_space)
-    assert False
+from merlin.pcvl_pytorch.noisy_slos import (
+    _InputStateNoisySLOSComputeGraph,
+    NoisySLOSComputeGraph,
+)
+from merlin.pcvl_pytorch.slos_torchscript import SLOSComputeGraph
